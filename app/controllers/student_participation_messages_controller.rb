009a84b795cd2b7a128a8ed6ee2a41909dd4b773
Stefan Berndtsson <stefan.berndtsson@ub.gu.se>
2019-10-24 12:50:08 +0200

    Better parsing of StudentParticipation

--- app/controllers/student_participation_messages_controller.rb
+++ app/controllers/student_participation_messages_controller.rb
@@ -1,12 +1,9 @@
 class StudentParticipationMessagesController < ApplicationController
   def create
-    pp request.raw_post
-    data = request.raw_post
-    data.force_encoding("UTF-8")
-    store_xml(data, "studiedeltagande")
-    data = reencode(data, "UTF-8")
-    student_participation = StudentParticipation.new(params)
-
+  	#log incoming message
+  	msg = Message.create(xml: request.raw_post, queue_name: "student_participation")
+    student_participation = StudentParticipation.new(params, msg)
+    student_participation.process_student_participation()
     render json: student_participation
   end
 end
--- app/models/student_participation.rb
+++ app/models/student_participation.rb
@@ -1,36 +1,19 @@
 class StudentParticipation
   include StudentParsingComponents
 
-  def initialize(data)
+  def initialize(data, msg)
     @raw = data
+    @msg = msg
     if !data["LadokParticipation"]
       raise StandardError, "Student message does not contain key: LadokParticipation"
     end
 
     parse(data["LadokParticipation"])
-
-    #student = self.as_json
-    #pp student extra pnr
-    my_student = @person
-    pp "EXTRA EXTRA READ ALL ABOUT IT"
-    pnr = my_student.extra[:pnr]
-    pp pnr
-    bdata = Koha.get_basic_data(pnr)
-    pp bdata[:uniq]
-    pp bdata[:borrowernumber]
-    pp bdata[:category_code]
-    # /personCanonical/anstallning/tomDatumSistaAnstallningsperiod/changed
-    pp bdata[:end_date_of_last_employment]
-    pp "=============================>>>>"
-
-    #Patron.get_basic_data(pnr)
-
-    # Send parsed data to Patron class, for temporary storage.
-    # If this completes the required data, the Patron class will
-    # write to ILS
-    ##Patron.store_student_participation(self.as_json)
   end
 
+  def process_student_participation
+  end
+  
   def as_json(opt = {})
     {
       person: @person.as_json(opt),
@@ -41,26 +24,59 @@ class StudentParticipation
   def parse(data)
     # Person data identical syntactically to Student message
     @person = Student.new(data)
+    @person_hash = @person.as_json()
+    @pnr = @person_hash[:pnr]
     @participation_type = data["type"]
-    @course = parse_course(data["courseSectionRecordSet"]["courseSectionRecord"]["courseSection"])
+    @course = parse_course(data["WithinCoursePackages"]["WithinCoursePackage"]["courseSectionRecordSet"]["courseSectionRecord"]["courseSection"])
   end
 
   def parse_course(data)
     main = parse_course_main(data)
     extra = parse_extension(data["extension"])
+
+    org_data = parse_org_data(data["WithinCoursePackages"]["WithinCoursePackage"]["courseTemplateRecord"],
+                              extra[:course_package_code])
+    
     {
       main: main,
       extra: extra,
+      org_data: org_data
     }
   end
 
+  def parse_org_data(data, package_code)
+    if !data.is_kind_of?(Array)
+      data = [data]
+    end
+
+    package = data.find do |pkg|
+      pkg["courseTemplate"]["courseNumber"]["textString"] == package_code
+    end
+
+    if !package
+      return {}
+    end
+
+    template = package["courseTemplate"]
+
+    orgname = nil
+    orgunit = nil
+    if template["org"]
+      orgname = template["org"]["orgName"] ? template["org"]["orgName"]["textString"] : nil
+      orgunit = template["org"]["id"] ? template["org"]["id"]["textString"] : nil
+    end
+    faculty = get_extension(template["extension"], "ResponsibleCommittee")
+    
+    {
+      orgname: orgname,
+      orgunit: orgunit,
+      faculty: faculty
+    }
+  end
+  
   def parse_course_main(data)
     label = data["label"] ? data["label"]["textString"] : nil
     status = data["status"]
-    if data["org"]
-      orgname = data["org"]["orgName"] ? data["org"]["orgName"]["textString"] : nil
-      orgunit = data["org"]["orgName"] ? data["org"]["orgUnit"]["textString"] : nil
-    end
     if data["timeFrame"]
       course_start = Time.parse(data["timeFrame"]["begin"])
       course_end = Time.parse(data["timeFrame"]["end"])
@@ -70,8 +86,6 @@ class StudentParticipation
     {
       label: label,
       status: status,
-      orgname: orgname,
-      orgunit: orgunit,
       course_start: course_start,
       course_end: course_end,
       location: location,
@@ -81,10 +95,11 @@ class StudentParticipation
   def parse_extension(extensiondata)
     educode = get_extension(extensiondata, "EducationTypeCode")
     course_title = get_extension(extensiondata, "CoursePackageTitle")
-
+    course_package_code = get_extension(extensiondata, "ParentTemplateCode")
     {
       educode: educode,
       course_title: course_title,
+      course_package_code: course_package_code
     }
   end
 
