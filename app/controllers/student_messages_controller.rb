3054b9d5f3117a56106725d5ed8b4fc498c7660b
Stefan Berndtsson <stefan.berndtsson@ub.gu.se>
2019-10-24 11:00:10 +0200

    Call the correct method when processing

--- app/controllers/student_messages_controller.rb
+++ app/controllers/student_messages_controller.rb
@@ -3,7 +3,7 @@ class StudentMessagesController < ApplicationController
   	#log incoming message
   	msg = Message.create(xml: request.raw_post, queue_name: "student")
     student = Student.new(params, msg)
-    student.process_card()
+    student.process_student()
     render json: student
   end
 end
