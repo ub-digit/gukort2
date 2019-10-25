3ef3338a222cbf52c15d70b8b3524bac14da74eb
Stefan Berndtsson <stefan.berndtsson@ub.gu.se>
2019-10-25 14:19:30 +0200

    Add support for Employee

--- app/controllers/employee_messages_controller.rb
+++ app/controllers/employee_messages_controller.rb
@@ -1,6 +1,9 @@
 class EmployeeMessagesController < ApplicationController
   def create
-    employee = Employee.new(params)
+  	#log incoming message
+  	msg = Message.create(xml: request.raw_post, queue_name: "employee")
+    employee = Employee.new(params, msg)
+    employee.process_employee()
     render json: employee
   end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,13 +1,89 @@
 class Employee
+  TEMPORARY_ACCOUNT_EXPIRATION = 2.months
 
-  def initialize(data)
+  def initialize(data, msg)
     @raw = data
+    @msg = msg
     if !data["personCanonical"]
       raise StandardError, "Employee message does not contain key: personCanonical"
     end
     parse(data["personCanonical"])
   end
 
+  def process_employee
+    begin
+      basic_data = Koha.get_basic_data(@pnr)
+    rescue => e
+      msg.append_response([__FILE__, __method__, __LINE__, e.message].inspect)
+    end
+    if basic_data[:borrowernumber]
+      process_update(basic_data)
+    else
+      process_create
+    end
+  end
+
+  def process_update(basic_data)
+    categorycode = basic_data[:categorycode]
+
+    # If Working for UB (PE) or already categorised as researcher (F*),
+    # do not replace category code, otherwise set GU as generic employee
+    if categorycode != "PE" && categorycode[0..0] != "F"
+      categorycode = "GU"
+    end
+
+    begin
+      Koha.update({
+        borrowernumber: basic_data[:borrowernumber],
+        patronuserid: @extra[:account],
+        categorycode: categorycode,
+        new_pnr: @new_pnr,
+        firstname: @name[:firstname],
+        surname: @name[:surname]
+      })
+    rescue => e
+      msg.append_response([__FILE__, __method__, __LINE__, e.message].inspect)
+    end
+
+  end
+
+  def process_create
+    if !IssuedState.has_issued_state?(@pnr)
+      IssuedState.set_issued_state(@pnr)
+      begin
+        MQ.generate_cardnumber(@pnr)
+      rescue => e
+        msg.append_response([__FILE__, __method__, __LINE__, e.message].inspect)
+      end
+    end
+
+    # Always set WR, and employee data lacks address information, so GNA is
+    # set as well.
+    debarments = ["wr", "gna"]
+    
+    begin
+      Koha.create({
+        origin: "gukort",
+        cardnumber: @pnr,
+        personalnumber: @pnr,
+        branchcode: "44",
+        debarments: debarments.join(","),
+        dateexpiry: Time.now + TEMPORARY_ACCOUNT_EXPIRATION,
+        patronuserid: @extra[:account],
+        firstname: @name[:firstname],
+        surname: @name[:surname],
+        phone: @contact[:phone],
+        email: @contact[:email],
+        categorycode: "GU",
+        lang: "sv-SE",
+        messaging_format: @contact[:email].present? ? "email" : nil,
+        accept_text: "Biblioteksreglerna accepteras"
+      })
+    rescue => e
+      msg.append_response([__FILE__, __method__, __LINE__, e.message].inspect)
+    end
+  end
+  
   def as_json(opt = {})
     {
       name: @name,
@@ -22,6 +98,8 @@ class Employee
     @name = parse_name(data)
     @contact = parse_contact(data)
     @extra = parse_extra(data)
+    @pnr = @extra[:pnr]
+    @new_pnr = @extra[:new_pnr]
   end
 
   def parse_name(data)
@@ -44,20 +122,28 @@ class Employee
 
   def parse_extra(data)
     pnr = deep_get(data, ["personnummer", "data"])
+    old_pnr = deep_get(data, ["personnummer", "previousData"])
+    if old_pnr
+      new_pnr = pnr
+      pnr = old_pnr
+    end
     account = deep_get(data, ["gukonto", "konto"])
     account_status = deep_get(data, ["gukonto", "status"])
     faculty_name = deep_get(data, ["anstallning", "fakultet", "organisationsnamn"])
     faculty_number = deep_get(data, ["anstallning", "fakultet", "organisationsnummer"])
     institution_name = deep_get(data, ["anstallning", "institution", "organisationsnamn"])
     institution_number = deep_get(data, ["anstallning", "institution", "organisationsnummer"])
+    last_employment_date = deep_get(data, ["anstallning", "tomDatumSistaAnstallningsperiod", "data"])
     {
       pnr: pnr,
+      new_pnr: new_pnr,
       account: account,
       account_status: account_status,
       faculty_name: faculty_name,
       faculty_number: faculty_number,
       institution_name: institution_name,
-      institution_number: institution_number
+      institution_number: institution_number,
+      last_employment_date: last_employment_date
     }
   end
 
@@ -65,7 +151,14 @@ class Employee
     d = data
     path.each do |p|
       return nil if !d[p]
-      d = d[p]
+      if d[p].kind_of?(Array)
+        d = d[p].first
+      else
+        d = d[p]
+      end
+    end
+    if d.kind_of?(Array)
+      return d.first
     end
     d
   end
