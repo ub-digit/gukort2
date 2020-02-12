0ff2fefa67ec61a61e20e3b45578cca9d928a601
Stefan Berndtsson <stefan.berndtsson@ub.gu.se>
2020-02-12 18:13:39 +0100

    Need to escape regexp properly

--- app/controllers/employee_messages_controller.rb
+++ app/controllers/employee_messages_controller.rb
@@ -2,7 +2,7 @@ class EmployeeMessagesController < ApplicationController
   def create
   	#log incoming message
     # Bypass ActionPack XML Parser, and remove xml encoding before use. It will always be UTF-8
-    input_xml = request.raw_post.gsub(/<?xml version=(.*) encoding=.*?>/, '<?xml version=\1?>')
+    input_xml = request.raw_post.gsub(/<\?xml version=(.*) encoding=.*\?>/, '<?xml version=\1?>')
     params = Hash.from_xml(input_xml)
     msg = Message.create(xml: input_xml, queue_name: "employee")
     employee = Employee.new(params, msg)
--- app/controllers/student_messages_controller.rb
+++ app/controllers/student_messages_controller.rb
@@ -2,7 +2,7 @@ class StudentMessagesController < ApplicationController
   def create
   	#log incoming message
     # Bypass ActionPack XML Parser, and remove xml encoding before use. It will always be UTF-8
-    input_xml = request.raw_post.gsub(/<?xml version=(.*) encoding=.*?>/, '<?xml version=\1?>')
+    input_xml = request.raw_post.gsub(/<\?xml version=(.*) encoding=.*\?>/, '<?xml version=\1?>')
     params = Hash.from_xml(input_xml)
     msg = Message.create(xml: input_xml, queue_name: "student")
     student = Student.new(params, msg)
