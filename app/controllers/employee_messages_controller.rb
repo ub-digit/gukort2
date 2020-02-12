ff5fb9d4d3d1cc1b374d14b90dce8ac06bf84914
Stefan Berndtsson <stefan.berndtsson@ub.gu.se>
2020-02-12 14:16:30 +0100

    Remove XML encoding from xml header, since anything other than UTF-8 will be invalid anyway. This will essentially reimplement actionpack_xml_parser, so it should probably be removed later.

--- app/controllers/employee_messages_controller.rb
+++ app/controllers/employee_messages_controller.rb
@@ -1,7 +1,10 @@
 class EmployeeMessagesController < ApplicationController
   def create
   	#log incoming message
-  	msg = Message.create(xml: request.raw_post, queue_name: "employee")
+    # Bypass ActionPack XML Parser, and remove xml encoding before use. It will always be UTF-8
+    input_xml = request.raw_post.gsub(/<?xml version=(.*) encoding=.*?>/, '<?xml version=\1?>')
+    params = Hash.from_xml(input_xml)
+    msg = Message.create(xml: input_xml, queue_name: "employee")
     employee = Employee.new(params, msg)
     employee.process_employee()
     render json: employee
--- app/controllers/student_participation_messages_controller.rb
+++ app/controllers/student_participation_messages_controller.rb
@@ -1,7 +1,10 @@
 class StudentParticipationMessagesController < ApplicationController
   def create
   	#log incoming message
-  	msg = Message.create(xml: request.raw_post, queue_name: "student_participation")
+    # Bypass ActionPack XML Parser, and remove xml encoding before use. It will always be UTF-8
+    input_xml = request.raw_post.gsub(/<?xml version=(.*) encoding=.*?>/, '<?xml version=\1?>')
+    params = Hash.from_xml(input_xml)
+    msg = Message.create(xml: input_xml, queue_name: "student_participation")
     student_participation = StudentParticipation.new(params, msg)
     student_participation.process_student_participation()
     render json: student_participation
