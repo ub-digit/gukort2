69fb2fe0a668a738c88f6b93be550775bbbf7a71
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2020-02-13 08:33:58 +0100

    Remove XML encoding from xml header, since anything other than UTF-8 … (#5)
    * Remove XML encoding from xml header, since anything other than UTF-8 will be invalid anyway. This will essentially reimplement actionpack_xml_parser, so it should probably be removed later.
    
    * Need to escape regexp properly

--- app/controllers/student_messages_controller.rb
+++ app/controllers/student_messages_controller.rb
@@ -1,7 +1,10 @@
 class StudentMessagesController < ApplicationController
   def create
   	#log incoming message
-  	msg = Message.create(xml: request.raw_post, queue_name: "student")
+    # Bypass ActionPack XML Parser, and remove xml encoding before use. It will always be UTF-8
+    input_xml = request.raw_post.gsub(/<\?xml version=(.*) encoding=.*\?>/, '<?xml version=\1?>')
+    params = Hash.from_xml(input_xml)
+    msg = Message.create(xml: input_xml, queue_name: "student")
     student = Student.new(params, msg)
     student.process_student()
     render json: student
